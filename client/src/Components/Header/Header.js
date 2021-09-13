c86cc5d1e723a598672c63f2564ede176e58c1a7
Ratko S <73665187+Ratko-Sim@users.noreply.github.com>
2021-09-13 15:35:11 +0200

    added logo, nav links, signin and register btns

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <div className="Header">
+    </div>
+  )
+}
+
+export default Header
--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import "./Navigation.scss"
+import { NavLink } from 'react-router-dom'
+import { RiUserShared2Fill } from 'react-icons/ri'
 
 const Navigation = () => {
   return (
-    <div>
-      Nav
+    <div className="navWrapper">
+      <span className="navLogo">free</span>
+      <ul className="navList">
+        <li className="navListItem">
+          <NavLink activeClassName="activePage" to="/home">
+            home
+          </NavLink>
+        </li>
+        <li className="navListItem">
+          <NavLink activeClassName="activePage" to="/categories">
+            categories
+          </NavLink>
+        </li>
+        <li className="navListItem">
+          <NavLink activeClassName="activePage" to="/contact">
+            contact
+          </NavLink>
+        </li>
+      </ul>
+      <div className="navSignInRegister">
+        <button className="signIn">
+          <div className="signInIcon">
+            <RiUserShared2Fill />
+          </div>
+          <span>Sign in</span>
+        </button>
+        <button className="register">register</button>
+      </div>
     </div>
   )
 }
