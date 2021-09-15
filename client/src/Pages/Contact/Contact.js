6720098228d0f790c7d01b1082b4ad5f3e6144ff
Brisilda Bushi <b.brisilda1552@gmail.com>
2021-09-15 10:50:07 +0200

    Adding contact us page and styling

--- client/src/Pages/Contact/Contact.js
+++ client/src/Pages/Contact/Contact.js
@@ -9,11 +9,11 @@ import {MdEmail} from "react-icons/md"
 const Contact = () => {
     return (
         <section className="contactContainer">
-            {/* <iframe
+            <iframe
                 className="map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d301941.965402527!2d13.144553707447377!3d52.50651327072207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2sBerlin!5e1!3m2!1sen!2sde!4v1631625218844!5m2!1sen!2sde"
-            ></iframe> */}
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.5606328089707!2d13.372827715756037!3d52.50956567981225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851c9727bffff%3A0x3e6ebf44d1faa00c!2sBahntower!5e1!3m2!1sen!2sde!4v1631636373346!5m2!1sen!2sde"></iframe>
+            ></iframe>
+            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.5606328089707!2d13.372827715756037!3d52.50956567981225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851c9727bffff%3A0x3e6ebf44d1faa00c!2sBahntower!5e1!3m2!1sen!2sde!4v1631636373346!5m2!1sen!2sde"></iframe> */}
             <div className="contactAllContainer">
     
                 <div className="contactFormContainer">
