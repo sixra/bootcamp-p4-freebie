aad9bd6773c49f8684036d622344d7d28b2c03d1
Ratko S <73665187+Ratko-Sim@users.noreply.github.com>
2021-09-16 10:56:00 +0200

    login and register modals

--- client/src/Redux/Reducers/RootReducer.js
+++ client/src/Redux/Reducers/RootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux"
 import { dropdownReducer } from "./DropdownReducer"
+import { ModalLoginReducer, ModalRegisterReducer } from "./ModalLoginAndRegister"
 
 const rootReducers = combineReducers({
   DropdownState: dropdownReducer,
+  LoginModalState: ModalLoginReducer,
+  RegisterModalState: ModalRegisterReducer
 });
 
 export default rootReducers;
