b9a3c65ca2f8fc3da43c00f91e8d900619a24d1c
Brisilda Bushi <b.brisilda1552@gmail.com>
2021-09-17 11:13:21 +0200

    Oopsie branchie not changed, fixed

--- client/src/Pages/Contact/Contact.js
+++ client/src/Pages/Contact/Contact.js
@@ -10,7 +10,7 @@ import HeroImage from "../../Components/Header/HeroImage"
 const Contact = () => {
     return (
         <section className="contactContainer">
-            {/* <HeroImage height="20" /> */}
+            <HeroImage height="20" />
             <div className="contactAllContainer">
 
                 <div className="contactFormContainer">
@@ -50,10 +50,7 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <iframe
-                className="map"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d301941.965402527!2d13.144553707447377!3d52.50651327072207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2sBerlin!5e1!3m2!1sen!2sde!4v1631625218844!5m2!1sen!2sde"
-            ></iframe>
+            <iframe className="map" src="https://www.mapquest.com/embed/germany/berlin-282238303?center=52.51716101425811,13.388900756835938&zoom=12&maptype=map"></iframe>
         </section>
     );
 };
