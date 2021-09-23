6344c43b7d1b864016982c7d0e7e82605b973571
Ratko S <73665187+Ratko-Sim@users.noreply.github.com>
2021-09-23 11:46:22 +0200

    categories page, pagination and styling fixes

--- client/src/Pages/Categories/Categories.js
+++ client/src/Pages/Categories/Categories.js
@@ -4,13 +4,24 @@ import SearchBar from '../../Components/Header/SearchBar'
 import { HiOutlineViewGrid } from "react-icons/hi"
 import { HiViewList } from "react-icons/hi"
 import { useDispatch, useSelector } from "react-redux";
-import { getAds } from "../../Redux/Actions/adsAction";
+import { getAds } from "../../Redux/Actions/AdsAction";
 import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner";
 import "./Categories.scss"
 import LoginModal from '../LoginLogout/LoginModal.js'
 import RegisterModal from '../LoginLogout/RegisterModal.js'
 import SingleAd from "./SingleAd";
 import { showGridView, hideGridView } from "../../Redux/Actions/ListGridView"
+import {
+  FaMobile,
+  FaTshirt,
+  FaBriefcase,
+  FaBabyCarriage,
+  FaConciergeBell,
+  FaHeart,
+} from "react-icons/fa";
+import { GiSofa, GiCarWheel, GiBookshelf, GiWatch } from "react-icons/gi";
+import { IoPawSharp, IoTvSharp } from "react-icons/io5";
+import ReactPaginate from "react-paginate";
 
 const Categories = () => {
 
@@ -35,6 +46,15 @@ const Categories = () => {
     dispatch(getAds());
   }, [dispatch]);
 
+  const [pageNumber, setPageNumber] = useState(0)
+  const postsPerPage = 10;
+  const visitedPages = pageNumber * postsPerPage;
+
+  const pageCount = Math.ceil(ads.length / postsPerPage)
+  const changePage = ({ selected }) => {
+    setPageNumber(selected)
+  }
+
   return (
     <div>
       <HeroImage height="25" minHeight="25" maxHeight="25" />
@@ -52,11 +72,18 @@ const Categories = () => {
             </div>
             <div>
               <ul>
-                <li>category one</li>
-                <li>category one</li>
-                <li>category one</li>
-                <li>category one</li>
-                <li>category one</li>
+                <li><GiCarWheel /> <span>vehicle Parts</span></li>
+                <li><GiSofa /> <span>furnitures</span></li>
+                <li><IoTvSharp /><span>electronics</span></li>
+                <li><FaMobile /> <span>mobiles</span></li>
+                <li><FaTshirt /> <span>clothing</span></li>
+                <li><GiWatch /> <span>accessories</span></li>
+                <li><FaBriefcase /> <span>jobs</span></li>
+                <li><FaConciergeBell /> <span>services</span></li>
+                <li><IoPawSharp /> <span>animals</span></li>
+                <li><GiBookshelf /> <span>books</span></li>
+                <li><FaBabyCarriage /> <span>baby products</span></li>
+                <li><FaHeart /> <span>matrimony</span></li>
               </ul>
             </div>
           </div>
@@ -74,11 +101,12 @@ const Categories = () => {
               <LoadingSpinner />
             ) : (
               <div className={gridView ? "latestAdsContain" : null}>
-                {ads.map((adInfo) => (
+                {ads.slice(visitedPages, visitedPages + postsPerPage).map((adInfo) => (
                   <SingleAd key={adInfo._id} adInfo={adInfo} />
                 ))}
               </div>
             )}
+            <ReactPaginate previousLabel={"<"} nextLabel={">"} pageCount={pageCount} onPageChange={changePage} containerClassName={"paginationBtn"} previousClassName={"previousPageBtn"} nextClassName={"nextPageBtn"} disabledClassName={"paginationDisabled"} activeClassName={"paginationActivePage"} />
           </div>
         </div>
       </section>
