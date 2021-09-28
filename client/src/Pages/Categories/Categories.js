b074d0acc57b51ba9c4c0b6e1f270423446f92b4
Ratko S <73665187+Ratko-Sim@users.noreply.github.com>
2021-09-28 20:11:59 +0200

    categories page, filter products by categories

--- client/src/Pages/Categories/Categories.js
+++ client/src/Pages/Categories/Categories.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
-import HeroImage from "../../Components/Header/HeroImage";
-import SearchBar from "../../Components/Header/SearchBar";
-import { HiOutlineViewGrid } from "react-icons/hi";
-import { HiViewList } from "react-icons/hi";
+import HeroImage from "../../Components/Header/HeroImage"
+import { HiOutlineViewGrid } from "react-icons/hi"
+import { HiViewList } from "react-icons/hi"
 import { useDispatch, useSelector } from "react-redux";
 import { getAds } from "../../Redux/Actions/AdsAction";
 import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner";
-import "./Categories.scss";
+import "./Categories.scss"
 import SingleAd from "./SingleAd";
-import { showGridView, hideGridView } from "../../Redux/Actions/ListGridView";
+import { showGridView, hideGridView } from "../../Redux/Actions/ListGridView"
 import {
   FaMobile,
   FaTshirt,
@@ -22,10 +21,11 @@ import { IoPawSharp, IoTvSharp, IoSearchOutline } from "react-icons/io5";
 import ReactPaginate from "react-paginate";
 
 const Categories = () => {
+
   const ads = useSelector((state) => state.ads);
   const dispatch = useDispatch();
 
-  const gridView = useSelector((state) => state.ListGridViewState);
+  const gridView = useSelector((state) => state.ListGridViewState)
 
   const showGrid = () => {
     dispatch(showGridView());
@@ -39,20 +39,18 @@ const Categories = () => {
     dispatch(getAds());
   }, [dispatch]);
 
-  const [pageNumber, setPageNumber] = useState(0);
+  const [pageNumber, setPageNumber] = useState(0)
   const postsPerPage = 4;
   const visitedPages = pageNumber * postsPerPage;
 
-  const pageCount = Math.ceil(ads.length / postsPerPage);
+  const pageCount = Math.ceil(ads.length / postsPerPage)
   const changePage = ({ selected }) => {
-    setPageNumber(selected);
-  };
+    setPageNumber(selected)
+  }
 
   return (
     <div>
       <HeroImage height="30" minHeight="25" maxHeight="25" />
-      <SearchBar />
-
       <section className="categoriesPageContainer">
         <div className="categoriesAndSearchContainer">
           <div className="searchBarContainer">
@@ -65,58 +63,31 @@ const Categories = () => {
             </div>
             <div className="categoriesListsContainer">
               <ul>
-                <li>
-                  <GiCarWheel /> <span>vehicle Parts</span>
-                </li>
-                <li>
-                  <GiSofa /> <span>furnitures</span>
-                </li>
-                <li>
-                  <IoTvSharp />
-                  <span>electronics</span>
-                </li>
+                <li><GiCarWheel /> <span>vehicle Parts</span></li>
+                <li><GiSofa /> <span>furnitures</span></li>
+                <li><IoTvSharp /><span>electronics</span></li>
               </ul>
               <ul>
-                <li>
-                  <FaMobile /> <span>mobiles</span>
-                </li>
-                <li>
-                  <FaTshirt /> <span>clothing</span>
-                </li>
-                <li>
-                  <GiWatch /> <span>accessories</span>
-                </li>
+                <li><FaMobile /> <span>mobiles</span></li>
+                <li><FaTshirt /> <span>clothing</span></li>
+                <li><GiWatch /> <span>accessories</span></li>
               </ul>
               <ul>
-                <li>
-                  <FaBriefcase /> <span>jobs</span>
-                </li>
-                <li>
-                  <FaConciergeBell /> <span>services</span>
-                </li>
-                <li>
-                  <IoPawSharp /> <span>animals</span>
-                </li>
+                <li><FaBriefcase /> <span>jobs</span></li>
+                <li><FaConciergeBell /> <span>services</span></li>
+                <li><IoPawSharp /> <span>animals</span></li>
               </ul>
               <ul>
-                <li>
-                  <GiBookshelf /> <span>books</span>
-                </li>
-                <li>
-                  <FaBabyCarriage /> <span>baby products</span>
-                </li>
-                <li>
-                  <FaHeart /> <span>matrimony</span>
-                </li>
+                <li><GiBookshelf /> <span>books</span></li>
+                <li><FaBabyCarriage /> <span>baby products</span></li>
+                <li><FaHeart /> <span>matrimony</span></li>
               </ul>
             </div>
           </div>
         </div>
         <div className="infoTabAndproductsList">
           <div className="infoTabContainer">
-            <span className="infoTabText">
-              You are seeing total of {ads.length} ads.
-            </span>
+            <span className="infoTabText">You are seeing total of {ads.length} ads.</span>
             <div className="gridListIconsContainer">
               <div>
                 <HiOutlineViewGrid onClick={showGrid} size={30} />
@@ -131,29 +102,19 @@ const Categories = () => {
               <LoadingSpinner />
             ) : (
               <div className={gridView ? "latestAdsContain" : null}>
-                {ads
-                  .slice(visitedPages, visitedPages + postsPerPage)
-                  .map((adInfo) => (
-                    <SingleAd key={adInfo._id} adInfo={adInfo} />
-                  ))}
+                {ads.slice(visitedPages, visitedPages + postsPerPage).map((adInfo) => (
+                  <SingleAd key={adInfo._id} adInfo={adInfo} />
+                ))}
               </div>
             )}
-            <ReactPaginate
-              previousLabel={"<"}
-              nextLabel={">"}
-              pageCount={pageCount}
-              onPageChange={changePage}
-              containerClassName={"paginationBtn"}
-              previousClassName={"previousPageBtn"}
-              nextClassName={"nextPageBtn"}
-              disabledClassName={"paginationDisabled"}
-              activeClassName={"paginationActivePage"}
-            />
+            <ReactPaginate previousLabel={"<"} nextLabel={">"} pageCount={pageCount} onPageChange={changePage} containerClassName={"paginationBtn"} previousClassName={"previousPageBtn"} nextClassName={"nextPageBtn"} disabledClassName={"paginationDisabled"} activeClassName={"paginationActivePage"} />
           </div>
         </div>
-      </section>
-    </div>
-  );
-};
+      </section >
 
-export default Categories;
+
+    </div >
+  )
+}
+
+export default Categories
