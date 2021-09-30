d9fe9690c5e9db795a299d4d58968a768ee5d67d
Ratko S <73665187+Ratko-Sim@users.noreply.github.com>
2021-09-30 09:14:49 +0200

    navigation links on user logged in, to dashboard

--- client/src/Components/Navigation/AuthButton/AuthButton.js
+++ client/src/Components/Navigation/AuthButton/AuthButton.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
-
+import { NavLink } from "react-router-dom";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { RiSendPlaneLine } from "react-icons/ri";
+import { BsLayers } from "react-icons/bs";
 import "./AuthButton.scss"
 
 const AuthButton = () => {
@@ -27,12 +31,40 @@ const AuthButton = () => {
       {user?.result ? (
         <div >
           <Link to="/user" alt={user?.result.name}>{user?.result.name}</Link>
+          <div className="userLinksDropdown">
+            <ul className="userSectionLinks">
+              <li>
+                <NavLink activeClassName="activeSection" to="/user/dashboard">
+                  <AiOutlineDashboard size={18} />
+                  <span> dashboard </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="activeSection" to="/user/post">
+                  <RiSendPlaneLine size={18} />
+                  <span> post an ad </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="activeSection" to="/user/ads">
+                  <BsLayers size={18} />
+                  <span> my ads </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="activeSection" to="/user/favorite">
+                  <AiOutlineHeart size={18} />
+                  <span> my favorites </span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
           <button onClick={logout}>Logout</button>
         </div>
       ) : (
         <>
-         <div className="signInContainer">
-          <Link to="/auth" >Sign In</Link>
+          <div className="signInContainer">
+            <Link to="/auth" >Sign In</Link>
           </div>
         </>
       )}
