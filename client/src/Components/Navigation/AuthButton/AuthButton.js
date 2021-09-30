81b5f821ec9699ea114dfcd4c0af907cb2e34f30
Ratko S <73665187+Ratko-Sim@users.noreply.github.com>
2021-09-30 10:06:35 +0200

    navigation links on user logged in, to dashboard

--- client/src/Components/Navigation/AuthButton/AuthButton.js
+++ client/src/Components/Navigation/AuthButton/AuthButton.js
@@ -7,6 +7,7 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { RiSendPlaneLine } from "react-icons/ri";
 import { BsLayers } from "react-icons/bs";
 import "./AuthButton.scss"
+//import { RiLogoutBoxLine } from "react-icons/ri"
 
 const AuthButton = () => {
 
@@ -29,37 +30,40 @@ const AuthButton = () => {
   return (
     <>
       {user?.result ? (
-        <div >
-          <Link to="/user" alt={user?.result.name}>{user?.result.name}</Link>
-          <div className="userLinksDropdown">
-            <ul className="userSectionLinks">
-              <li>
-                <NavLink activeClassName="activeSection" to="/user/dashboard">
-                  <AiOutlineDashboard size={18} />
-                  <span> dashboard </span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink activeClassName="activeSection" to="/user/post">
-                  <RiSendPlaneLine size={18} />
-                  <span> post an ad </span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink activeClassName="activeSection" to="/user/ads">
-                  <BsLayers size={18} />
-                  <span> my ads </span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink activeClassName="activeSection" to="/user/favorite">
-                  <AiOutlineHeart size={18} />
-                  <span> my favorites </span>
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-          <button onClick={logout}>Logout</button>
+        <div className="userSectionDropdown" >
+          <Link className="userSectionDropdownBtn" to="/user" alt={user?.result.name}>{user?.result.name.slice(0, 1).toUpperCase()}</Link>
+          <ul className="userSectionLinks">
+            <li>
+              <NavLink className="listContainer" activeClassName="activeSection" to="/user/dashboard">
+                <AiOutlineDashboard size={18} />
+                <span> dashboard </span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="listContainer" activeClassName="activeSection" to="/user/post">
+                <RiSendPlaneLine size={18} />
+                <span> post an ad </span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="listContainer" activeClassName="activeSection" to="/user/ads">
+                <BsLayers size={18} />
+                <span> my ads </span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="listContainer" activeClassName="activeSection" to="/user/favorite">
+                <AiOutlineHeart size={18} />
+                <span> my favorites </span>
+              </NavLink>
+            </li>
+            <li>
+              <div className="listContainer">
+                {/* <RiLogoutBoxLine size={18} /> */}
+                <button className="userLogoutBtn" onClick={logout}>Logout</button>
+              </div>
+            </li>
+          </ul>
         </div>
       ) : (
         <>
