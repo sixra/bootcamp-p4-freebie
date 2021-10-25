a2bafe37f85b586bcc2f144ea3d5100af2c91316
Heman-Development <hemandevelopment@gmail.com>
2021-10-25 13:59:13 +0200

    make it better

--- client/src/Api/Api.js
+++ client/src/Api/Api.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000" });
+// const API = axios.create({ baseURL: "http://localhost:4000" });
+const API = axios.create({ baseURL: "/" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
-    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
-      }`;
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
   }
   return req;
 });
@@ -16,7 +18,8 @@ export const fetchAds = () => API.get("api/items");
 export const fetchAd = (id) => API.get(`api/items/${id}`);
 export const postAd = (newAd) => API.post("api/items", newAd);
 export const deleteAd = (_id) => API.delete(`api/items/${_id}`);
-export const updateAd = (_id, updateItem) => API.patch(`api/items/${_id}`, updateItem);
+export const updateAd = (_id, updateItem) =>
+  API.patch(`api/items/${_id}`, updateItem);
 
 //////// API FOR SIGN IN - SIGN UP ////////
 
