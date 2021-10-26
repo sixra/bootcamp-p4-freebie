907eb1e2e54d707929088b2e65ecbd426d4f9ce7
Heman-Development <hemandevelopment@gmail.com>
2021-10-26 13:56:09 +0200

    active

--- client/src/Api/activate/user/hash.js
+++ client/src/Api/activate/user/hash.js
@@ -2,7 +2,7 @@ export async function activateUser(hash) {
   console.log("activateUser");
   const response = await fetch(
     // `http://localhost:4000/api/activate/user/${hash}`
-    `/api/activate/user/${hash}`
+    `${process.env.DOMAIN}/api/activate/user/${hash}`
   );
   console.log("activateUser2");
   if (response.status >= 400) {
