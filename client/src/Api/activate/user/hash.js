1fea34503fa3a26514d6c6c181774e970d79c3ed
Heman-Development <hemandevelopment@gmail.com>
2021-10-26 10:48:31 +0200

    debugf

--- client/src/Api/activate/user/hash.js
+++ client/src/Api/activate/user/hash.js
@@ -1,9 +1,10 @@
 export async function activateUser(hash) {
+  console.log("activateUser");
   const response = await fetch(
     // `http://localhost:4000/api/activate/user/${hash}`
     `/api/activate/user/${hash}`
   );
-
+  console.log("activateUser2");
   if (response.status >= 400) {
     return { message: "Cannot Validate an User!" };
   } else {
--- controllers/user.js
+++ controllers/user.js
@@ -71,8 +71,8 @@ export const signup = async (req, res) => {
     const savedUser = await newUser.save();
 
     const hash = savedUser._id;
-    // const url = `${process.env.DOMAIN}/Api/activate/user/${hash}`;
-    const url = `/api/activate/user/${hash}`;
+    const url = `${process.env.DOMAIN}/Api/activate/user/${hash}`;
+    // const url = `/api/activate/user/${hash}`;
     // const url = `/${hash}`
 
     await sendConfirmationEmail({
